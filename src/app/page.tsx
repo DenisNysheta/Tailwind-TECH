937bccea3423ff8f7ae68da7dd791f5ff1ea9cc5
Denis <nisetadenis@gmail.com>
2024-09-20 16:10:53 +0300

    feat: add new padding for section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main>
       <Header />
-      <section className="bg-[#F8F9F9] gap-[48px] h-[92vh] flex flex-col items-center p-[40px_24px_24px_24px]">
+      <section className="bg-[#F8F9F9] gap-[48px] h-[92vh] flex flex-col items-center p-[40px_24px_40px_24px]">
           <Verification />
           <Accordion />
       </section>
